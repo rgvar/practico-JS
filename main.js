0f906d9e7abbd91047e21f71d27da45967fc085b
Rodrigo <vargasrodrigoluis@gmail.com>
2024-09-17 18:18:37 -0300

    punto 4 y 5

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 import './style.css'
 
 
-// INTRODUCCIÓN
-// ejercicio 2
+// 1.INTRODUCCIÓN
+// ejercicio 1
 const ejercicioIntro1 = () => {
     let a = 5;
     let b = 8;
     let c = a + b;
     console.log('La suma de a y b es: ', c);
 };
-// ejercicio 3
+
+let buttonIntro1 = document.getElementById('button-intro-1');
+
+buttonIntro1.addEventListener('click', () => {
+    ejercicioIntro1();
+});
+
+// ejercicio 2
 const ejercicioIntro2 = () => {
     let input = prompt('¿Cuál es tu nombre?');
     console.log('Buen día ' + input + '.');
 };
 
-// OPERADORES LÓGICOS Y CONDICIONALES
+let buttonIntro2 = document.getElementById('button-intro-2');
+
+buttonIntro2.addEventListener('click', () => {
+    ejercicioIntro2();
+});
+
+// 2.OPERADORES LÓGICOS Y CONDICIONALES
 // ejercicio 1
 const ejercicioOperadores1 = () => {
     let a = 10;
@@ -28,6 +41,14 @@ const ejercicioOperadores1 = () => {
         console.log('a es menor que b')
     }
 };
+
+let buttonOperadores1 = document.getElementById('button-operadores-1');
+
+buttonOperadores1.addEventListener('click', () => {
+    ejercicioOperadores1();
+});
+
+//ejercicio 2
 const ejercicioOperadores2 = () => {
     let a = prompt('Ingrese un número: ');
     if (a % 2 == 0) {
@@ -37,7 +58,13 @@ const ejercicioOperadores2 = () => {
     }
 };
 
-// OPERADORES DE ASIGNACIÓN Y BUCLES
+let buttonOperadores2 = document.getElementById('button-operadores-2');
+
+buttonOperadores2.addEventListener('click', () => {
+    ejercicioOperadores2();
+});
+
+// 3.OPERADORES DE ASIGNACIÓN Y BUCLES
 // ejercicio 1
 const ejercicioBucles1 = () => {
     let a = 10;
@@ -46,48 +73,88 @@ const ejercicioBucles1 = () => {
         a--;
     }
 };
+
+let buttonBucles1 = document.getElementById('button-bucles-1');
+
+buttonBucles1.addEventListener('click', () => {
+    ejercicioBucles1();
+});
+
 const ejercicioBucles2 = () => {
     let a;
     do {
         a = prompt('Ingrese un número mayor a 100: ');
     } while (a <= 100);
     console.log('El número ' + a + ' es mayor a 100. ');
-}
+};
 
-
-
-// INTRODUCCIÓN
-let buttonIntro1 = document.getElementById('button-intro-1');
-let buttonIntro2 = document.getElementById('button-intro-2');
-
-buttonIntro1.addEventListener('click', () => {
-    ejercicioIntro1();
-});
-
-buttonIntro2.addEventListener('click', () => {
-    ejercicioIntro2();
-});
-
-// OPERADORES
-let buttonOperadores1 = document.getElementById('button-operadores-1');
-let buttonOperadores2 = document.getElementById('button-operadores-2');
-
-buttonOperadores1.addEventListener('click', () => {
-    ejercicioOperadores1();
-})
-
-buttonOperadores2.addEventListener('click', () => {
-    ejercicioOperadores2();
-})
-
-// BUCLES Y ASIGNACIONES
-let buttonBucles1 = document.getElementById('button-bucles-1');
 let buttonBucles2 = document.getElementById('button-bucles-2');
-
-buttonBucles1.addEventListener('click', () => {
-    ejercicioBucles1();
-})
 
 buttonBucles2.addEventListener('click', () => {
     ejercicioBucles2();
-})
+});
+
+// 4.FUNCIONES
+// ejercicio 1
+const esPar = (a) => {
+    if (a % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+let buttonFuncion1 = document.getElementById('button-funcion-1');
+
+buttonFuncion1.addEventListener('click', () => {
+    console.log(esPar(40), esPar(21), esPar(4));
+});
+
+// ejercicio 2
+const convertirCelsiusAFahrenheit = (celcius) => {
+    return celcius * 1.8 + 32;
+};
+
+let buttonFuncion2 = document.getElementById('button-funcion-2');
+
+buttonFuncion2.addEventListener('click', () => {
+    let grados = 30;
+    console.log(grados + '°C es igual a ' + convertirCelsiusAFahrenheit(grados) + '°F');
+});
+
+// 5. OBJETOS
+// ejercicio 1
+const persona = {
+    nombre: 'Rodrigo',
+    edad: 25,
+    ciudad: 'Mendoza',
+    setCiudad: (nuevaCiudad) => {
+        persona.ciudad = nuevaCiudad;
+    }
+};
+
+let buttonObjeto1 = document.getElementById('button-objeto-1');
+
+buttonObjeto1.addEventListener('click', () => {
+    console.log(persona);
+    persona.setCiudad('Tierra del Fuego');
+    console.log(persona);
+});
+
+// ejercicio 2
+const libro = {
+    titulo: 'Código Limpio',
+    autor: 'Robert C. Martin',
+    año: 2012,
+    esAntiguo: () => {
+        if (año > 10) 
+            return true;
+        else return false;
+    }
+}
+
+let buttonObjeto2 = document.getElementById('button-objeto-2');
+
+buttonObjeto2.addEventListener('click', () => {
+    console.log('El libro tiene más de 10 años? ', );
+});
